78aa1a484fc5618f9b807915426bf33d91d4c005
MayangMuzi <mayang.mauzi@gmail.com>
2020-10-28 15:31:12 +0700

    menambahkan latihan pertemuan6

--- TUGAS3/Pertemuan6/latihan2.php
+++ TUGAS3/Pertemuan6/latihan2.php
@@ -1,7 +1,7 @@
 <?php
 $link = mysqli_connect("localhost", "root", "");
 $dbname = "lat dbase";
-$cek = mysqli_query("CREATE DATABASE $dbname")
+$cek = mysqli_query($link, $query, $resultmode = "CREATE DATABASE $dbname")
   or die("Gagal Membuat Database : $dbname");
 
 if ($cek) {

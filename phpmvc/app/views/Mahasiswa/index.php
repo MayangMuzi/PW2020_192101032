e3d0d8f2eec7f52eceb0061aac894e9d364baa1a
MayangMuzi <mayang.mauzi@gmail.com>
2020-12-23 12:21:06 +0700

    menambahkan tambah data mahasiswa

--- phpmvc/app/views/Mahasiswa/index.php
+++ phpmvc/app/views/Mahasiswa/index.php
@@ -12,43 +12,47 @@
     <div class="row">
       <div class="col-6">
         <div class="row">
-          <div class="col-lg-6"> <?php Flasher::flash(); ?> </div>
+          <div class="col-lg-6">
+            <?php Flasher::flash(); ?>
+          </div>
         </div>
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
+          Tambah Data Mahasiswa </button>
+        <br><br>
+
         <!-- Modal -->
-        <div class="modal fade" id="formModal" tabindex="-1" aria-labelledby="judlModal" aria-hidden="true">
+        <div class="modal fade" id="formModal" tabindex="-1" arialabelledby="judulModal" aria-hidden="true">
           <div class="modal-dialog">
             <div class="modal-content">
               <div class="modal-header">
                 <h5 class="modal-title" id="judulModal">Tambah Data Mahasiswa</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close">
-                </button>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" arialabel="Close"></button>
               </div>
               <div class="modal-body">
-                <form action="<?= BASEURL; ?>/mahasiswa/tambah" method="post">
+                <div class="form-group">
+                  <label for="nama">Nama</label>
+                  <input type="text" class="form-control" id="nama" name="nama">
+                </div>
 
-                  <div class="form-group">
-                    <label for="nama">Nama</label>
-                    <input type="text" class="form-control" id="nama" name="nama">
-                  </div>
+                <div class="form-group"> <label for="nim">NIM</label>
+                  <input type="number" class="form-control" id="nim" name="nim">
+                </div>
 
-                  <div class="form-group"> <label for="nim">NIM</label>
-                    <input type="number" class="form-control" id="nim" name="nim">
-                  </div>
+                <div class="form-group"> <label for="email">Email</label>
+                  <input type="email" class="form-control" id="email" name="email">
+                </div>
 
-                  <div class="form-group"> <label for="email">Email</label>
-                    <input type="email" class="form-control" id="email" name="email">
-                  </div>
-
-                  <div class="form-group">
-                    <label for="jurusan">Jurusan</label>
-                    <select class="form-control" name="jurusan" id="jurusan">
-                      <option value="Teknik Informatika">Teknik Informatika</option>
-                      <option value="Sistem Informasi">Sistem Informasi</option>
-                      <option value="Teknik Industri">Teknik Industri</option>
-                      <option value="Teknik Kimia">Teknik Kimia</option>
-                      <option value="Teknik Elektro">Teknik Elektro</option>
-                    </select>
-                  </div>
+                <div class="form-group">
+                  <label for="jurusan">Jurusan</label>
+                  <select class="form-control" name="jurusan" id="jurusan">
+                    <option value="Teknik Informatika">Teknik Informatika</option>
+                    <option value="Sistem Informasi">Sistem Informasi</option>
+                    <option value="Teknik Industri">Teknik Industri</option>
+                    <option value="Teknik Kimia">Teknik Kimia</option>
+                    <option value="Teknik Elektro">Teknik Elektro</option>
+                  </select>
+                </div>
               </div>
 
               <div class="modal-footer">
@@ -59,11 +63,6 @@
             </div>
           </div>
         </div>
-
-        <!-- Button trigger modal -->
-        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#formModal">
-          Tambah Data Mahasiswa </button>
-        <br><br>
 
         <h3>Daftar Mahasiswa</h3>
         <ul class="list-group">
@@ -77,8 +76,6 @@
       </div>
     </div>
   </div>
-
-
 </body>
 
 </html>

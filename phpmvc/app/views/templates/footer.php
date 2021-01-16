b9d8982741733aa0ef8d629d4b89dc680c83a104
MayangMuzi <mayang.mauzi@gmail.com>
2021-01-16 09:32:16 +0700

    menambahkan hapus dan ubah

--- phpmvc/app/views/templates/footer.php
+++ phpmvc/app/views/templates/footer.php
@@ -6,6 +6,12 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
   <script src="<?= BASEURL; ?> /js/bootstrap.js"></script>
+  <script src="https://code.jquery.com/jquery-3.2.1.min.js"></script>
+  <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
+  <script src="<?= BASEURL; ?>/js/script.js"></script>
+  </body>
+
+</html>
 </head>
 
 <body>
